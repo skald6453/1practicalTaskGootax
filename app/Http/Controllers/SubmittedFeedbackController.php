942303552ce4491c913ca50ad7edf5d3d7e8d6ed
Семёнов Павел <pavel-6453.pavlov@yandex.ru>
2023-09-18 16:15:22 +0400

    Сделал валидацию полей на форме обратной связи

---
+++ app/Http/Controllers/SubmittedFeedbackController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Feedback;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SubmittedFeedbackController extends Controller
+{
+
+    protected function create(Request $request){
+
+        $validated= $request->validate([
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'feedback' => ['required', 'string', 'min:10'],
+        ]);
+
+        $rules = Feedback::create([
+            'email' => $request->email,
+            'sex' => $request->sex,
+            'delivery' => $request->delivery,
+            'feedback' => $request->feedback,
+        ]);
+
+        return view('submittedFeedback');
+    }
+
+}
--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -14,9 +14,16 @@
                     <div class="col">
 
                         <label class="text p-4" style="font-family: 'Nunito', sans-serif; font-size: 18px;">Ваш e-mail</label>
-                        <input class="form-control w-50" name="email" style="margin-left: 25%;">
+                        <input class="form-control w-50 @error('email') is-invalid @enderror"  name="email" style="margin-left: 25%;">
+
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
                         <label class="text-dark p-4" style="font-family: 'Nunito', sans-serif; font-size: 18px;">Пол</label>
+
                         <div class="col">
                         <input type="radio" value="Мужской" name="sex" class="btn-check" id="option1" autocomplete="off" checked>
                         <label class="btn btn-secondary" for="option1" >Мужской</label>
@@ -24,18 +31,22 @@
                         <input type="radio" value="Женский" name="sex" class="btn-check" id="option2" autocomplete="off">
                         <label class="btn btn-secondary" for="option2">Женский</label>
                         </div>
+
                         <label class="text-dark p-4" style="font-family: 'Nunito', sans-serif; font-size: 18px;">Способ доставки</label>
+
                         <select class="form-select" name="delivery">
                             <option selected>Курьер</option>
                             <option value="Самовывоз">Самовывоз</option>
                             <option value="Доставка на пункт выдачи">Доставка на пункт выдачи</option>
                         </select>
+
                         <div class="form-check p-5">
                             <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
                             <label class="form-check-label" for="flexCheckChecked">
                                 Вы принимаете политику конфиденциальности и условия использования
                             </label>
                         </div>
+
                         <div class="btn">
                             <button type="submit" class="btn btn-outline-success">Отправить форму</button>
 
@@ -45,17 +56,18 @@
 
                         <div class="col">
                             <label>Напишите отзыв</label>
-                            <textarea class="form-control" name="feedback" placeholder="Введите текст..."></textarea>
+                            <textarea class="form-control @error('feedback') is-invalid @enderror" name="feedback" placeholder="Введите текст..."></textarea>
+
+                            @error('feedback')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{($message)}}</strong>
+                                </span>
+                            @enderror
                         </div>
-
-
                     </div>
                     </form>
                 </div>
-
             </div>
         </div>
     </div>
-
-
 @endsection
